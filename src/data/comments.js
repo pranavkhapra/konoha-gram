ae2d7b0d01f83a9bc381fe5afdbf86930825096c
pranavkhapra <pranavkhapra23@gmail.com>
2021-08-03 14:37:18 +0530

    adding the data for now

---
+++ src/data/comments.js
@@ -0,0 +1,10 @@
+const comments = {
+  postid1: [
+    {
+      text: 'Totally need to try this.',
+      user: 'heavymetaladam',
+    },
+  ],
+};
+
+export default comments;
---
+++ src/data/posts.js
@@ -0,0 +1,11 @@
+const posts = [
+  {
+    code: 'postid1',
+    caption: 'A Fresh Morning in the Village',
+    likes: 150,
+    id: '1',
+    display_src: `https://i.ibb.co/GkQ5XZF/66990090.jpg`,
+  },
+];
+
+export default posts;
